f0e93c81a34d05ae2e17c6207fd8f62fd02e8625
Ben <40634403+benmaxlevy@users.noreply.github.com>
2021-07-16 18:15:57 -0400

    Update config.json.sh

--- config.json.sh
+++ config.json.sh
@@ -4,13 +4,6 @@ cat >config.json <<!SUB!THIS!
 {
   "discord": {
     "token": "$DISCORD_TOKEN"
-  },
-  "database": {
-    "host": "$DB_HOST",
-    "port": $DB_PORT,
-    "username": "$DB_USERNAME",
-    "password": "$DB_PASSWORD",
-    "database": "$DB_DATABASE"
   }
 }
 !SUB!THIS!
